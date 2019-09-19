7c918b97ac0fdf819e8ab3cd5c20411e66264377
Kunwar Raj Singh <kunwar31@gmail.com>
2019-09-19 20:27:04 +0530

    added skeletal active_learner and oracle

---
+++ active_learner.py
@@ -0,0 +1,82 @@
+import numpy as np
+from typing import List
+from flair.data import Sentence
+from flair.trainers import ModelTrainer
+from flair.optim import AdamW
+
+
+class ActiveLearner:
+    def __init__(self,
+                 sentences: List[Sentence],
+                 experiment_name: str,
+                 data_path: str,
+                 oracle,
+                 embeddings_storage_mode='cpu',
+                 ):
+        self.data_path = data_path
+        self.sentences = sentences
+        self.oracle = oracle
+        self.experiment_name = experiment_name
+        self.embeddings_storage_mode = embeddings_storage_mode
+
+    def train_model(self, corpus, classifier, optimizer_state=None, epoch=1, lr=1e-5):
+        trainer = ModelTrainer(classifier, corpus, optimizer=AdamW, optimizer_state=optimizer_state)
+        result = trainer.train(f'{self.data_path}/{self.experiment_name}/',
+                               learning_rate=lr,
+                               min_learning_rate=1e-8,
+                               mini_batch_size=32,
+                               anneal_factor=0.5,
+                               patience=5,
+                               max_epochs=epoch,
+                               embeddings_storage_mode=self.embeddings_storage_mode,
+                               use_amp=True,
+                               weight_decay=1e-4,)
+
+        return classifier, result['optimizer_state']
+
+    @staticmethod
+    def get_predictions(data, classifier, mini_batch_size=1024):
+        return [(s.labels[0].value,
+                 s.labels[0].score) for s in classifier.predict(data, mini_batch_size=mini_batch_size)]
+
+    def sample_confused_samples(self, classifier, labelling_step_size, sampling_multiplier, max_sample_size):
+        train_data_sample_size = min(labelling_step_size * sampling_multiplier, max_sample_size)
+        if len(self.sentences) > train_data_sample_size:
+            random_indices = np.random.choice(a=len(self.sentences), size=train_data_sample_size, replace=False)
+            sentence_sample = []
+            for index in random_indices:
+                sentence_sample.append(self.sentences[index])
+        else:
+            sentence_sample = self.sentences.copy()
+        predictions = self.get_predictions(sentence_sample, classifier)
+        weights = []
+        for (label, pred) in predictions:
+            weights.append(1 - abs(0.5-pred))
+        weights = np.array(weights)
+        weights = (weights * 2) - 1
+        weights = weights / weights.sum()
+        confused_idx = list(np.random.choice(a=len(predictions),
+                                             size=labelling_step_size,
+                                             p=weights,
+                                             replace=False))
+        return [sentence_sample[idx] for idx in confused_idx]
+
+    def step(self,
+             classifier,
+             optimizer_state=None,
+             labelling_step_size: int = 100,
+             sampling_multiplier: int = 100,
+             max_sample_size: int = 10000,
+             step_lr=1e-5):
+
+        confusion_samples = self.sample_confused_samples(classifier,
+                                                         labelling_step_size,
+                                                         sampling_multiplier, max_sample_size)
+        corpus = self.oracle.get_labelled_corpus(confusion_samples, self.experiment_name)
+        classifier, opt_state = self.train_model(corpus, classifier, optimizer_state, lr=step_lr)
+        return classifier, opt_state
+
+
+
+
+
---
+++ oracle.py
@@ -0,0 +1,84 @@
+from typing import List
+from flair.data import Sentence
+from time import time
+
+
+class Oracle:
+    def get_label(self, sample: Sentence):
+        raise NotImplementedError
+
+
+class MemoryOracle(Oracle):
+    def __init__(self, memory):
+        if memory is None:
+            self.memory = {}
+        else:
+            self.memory = memory
+
+    def _read_all_labelled_data(self):
+        pass
+
+    def get_label(self, sample):
+        if sample in self.memory:
+            return self.memory[sample]
+
+
+class RuledOracle(Oracle):
+    def __init__(self, function):
+        self.function = function
+
+    def get_label(self, sample):
+        return self.function(sample)
+
+
+class HumanOracle(Oracle):
+    def get_label(self, sample):
+        print(sample)
+        print('Y/N - Y for correct N for wrong')
+        choice = input()
+        if choice.lower() == 'y':
+            return 1
+        else:
+            return 0
+
+
+class MultiSourceOracle(Oracle):
+    def __init__(self, all_data_path,
+                 valid_path,
+                 test_path,
+                 use_memory=True,
+                 use_rules=False,
+                 memory=None,
+                 rule_func=lambda x: 1):
+        self.all_data_path = all_data_path
+        self.valid_path = valid_path
+        self.test_path = test_path
+        self.use_memory = use_memory
+        self.use_rules = use_rules
+        if self.use_memory:
+            self.memory_oracle = MemoryOracle(memory)
+        if self.use_rules:
+            self.ruled_oracle = RuledOracle(rule_func)
+        self.human_oracle = HumanOracle()
+
+    def get_label(self, sample):
+        label = None
+        if self.use_memory:
+            label = self.memory_oracle.get_label(sample)
+        if self.use_rules:
+            if label is None:
+                label = self.ruled_oracle.get_label(sample)
+        if label is None:
+            label = self.human_oracle.get_label(sample)
+        return label
+
+    def get_labelled_corpus(self, samples: List[Sentence], experiment_name):
+        labels = []
+        for sample in samples:
+            labels.append(self.get_label(sample))
+        with open(f'{experiment_name}/labelled_{len(samples)}_{int(time())}.csv', 'w') as f:
+            for idx in range(len(samples)):
+                f.write(f'{samples[idx].to_plain_string()}, {labels[idx]}')
+
+
+
